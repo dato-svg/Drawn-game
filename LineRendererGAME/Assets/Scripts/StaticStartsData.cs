19ba7bd4abac1d4658bdb97b36fd3e59e3de56cf
dato-svg <139239441+dato-svg@users.noreply.github.com>
2024-03-26 16:27:39 +0400

    save
    save

--- LineRendererGAME/Assets/Scripts/StaticStartsData.cs
+++ LineRendererGAME/Assets/Scripts/StaticStartsData.cs
@@ -5,8 +5,8 @@ namespace DefaultNamespace
     [Serializable]
     public class StaticStartsData
     {
-        public static int[] isOpen = new int [31];
-        public static int[] starCount = new int[31];
+        public static int[] isOpen = new int [61];
+        public static int[] starCount = new int[61];
     }
     
 }
